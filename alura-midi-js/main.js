8aed185d5f45a384cb469918a05910a102d5e7b8
Gabriel Pupo <gabrielpupo21@gmail.com>
2023-10-15 15:22:53 -0300

    first commit

---
+++ alura-midi-js/main.js
@@ -0,0 +1,35 @@
+function tocaSom (selectorAudio) {
+    const elemento = document.querySelector(selectorAudio);
+    
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        alert('Elemento não encontrado')
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
