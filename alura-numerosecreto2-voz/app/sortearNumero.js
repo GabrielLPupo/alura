545a9e44555ed9d0366ff7e0246bc19ecef506c3
Gabriel Pupo <gabrielpupo21@gmail.com>
2023-11-11 04:11:54 -0300

    update

---
+++ alura-numerosecreto2-voz/app/sortearNumero.js
@@ -0,0 +1,15 @@
+const menorValor = 1
+const maiorValor = 10
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log('ඞ é:',numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
